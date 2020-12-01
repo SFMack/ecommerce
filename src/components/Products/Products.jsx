5e1956dd8ff938c2dfb06488af82a982705d939f
SFMack <sfcamack@icloud.com>
2020-11-30 19:00:49 -0800

    update shopping cart functionality

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,33 +2,18 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Soap",
-    description: "Clean smelling soap",
-    price: "$7",
-    image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-  },
-  {
-    id: 2,
-    name: "Lotion",
-    description: "Clean smelling lotion",
-    price: "$9",
-    image:
-      "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  },
-];
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
-const Products = () => {
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
